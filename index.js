804c9ee3fc502df6313624cd92567867b22bda78
Bishal Dofo <dofobishal@gmail.com>
2023-10-20 13:23:18 +0600

    Product add tested

--- index.js
+++ index.js
@@ -25,10 +25,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
      await client.connect();
      
+     const productCollection = client.db('productDB').collection('product')
 
      app.post('/products', async (req, res) => {
         const newProduct = req.body;
-        console.log(newProduct)
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
      })
    
     // Send a ping to confirm a successful connection
